137bb2415ca29058db4cbd969a147207d381d658
Tyler Dixon <onyxqk@gmail.com>
2023-12-28 00:16:45 -0500

    implemented new approach for adding items/lanes

--- orbital/board/board.js
+++ orbital/board/board.js
@@ -1,114 +1,118 @@
-const board = document.getElementById('board'),
-    addItemForm = document.getElementById('add-item-form'),
-    addItemButton = document.getElementById('add-item-button'),
-    saveAddItemButton = document.getElementById('save-add-item-button'),
-    cancelAddItemButton = document.getElementById('cancel-add-item-button'),
-    addItemInput = document.getElementById('add-item-input'),
-    addLaneForm = document.getElementById('add-lane-form'),
-    addLaneButton = document.getElementById('add-lane-button'),
-    saveAddLaneButton = document.getElementById('save-add-lane-button'),
-    cancelAddLaneButton = document.getElementById('cancel-add-lane-button'),
-    addLaneInput = document.getElementById('add-lane-input'),
-    body = document.getElementById('body');
-    
-function addItem() {
-    addItemForm.style.display = 'block';
-}
+function addItem(button) {
+    let lane = button.parentNode
+    let newCard = document.createElement('div')
+    newCard.className = 'card'
+    newCard.tabIndex = 0
 
-function cancelAddItem() {
-    addItemForm.style.display = 'none';
-}
+    let itemHeader = document.createElement('span')
+    itemHeader.className = 'item-header'
+    itemHeader.contentEditable = true
+    itemHeader.textContent = 'New Item'
 
-function saveAddItem() {
-    const itemText = addItemInput.value;
-    if (!itemText) return;
-    const newCard = document.createElement('div');
-    newCard.classList.add('card');
-    newCard.innerText = itemText;
-    board.firstElementChild.appendChild(newCard);
+    let removeButton = document.createElement('button')
+    removeButton.className = 'remove-item-button'
+    removeButton.title = 'Remove Item'
+    removeButton.textContent = 'X'
+    removeButton.onclick = function() {
+        removeItem(this)
+    }
 
-    addItemForm.style.display = 'none';
-    addItemInput.value = '';
+    newCard.appendChild(itemHeader)
+    newCard.appendChild(removeButton)
+
+    lane.appendChild(newCard)
+    lane.insertBefore(newCard, button)
     makeItemDraggable(newCard)
 }
 
 function addLane() {
-    addLaneForm.style.display = 'block';
+    let board = document.getElementById('board')
+    let newLane = document.createElement('div')
+    newLane.className = 'lane'
 
-}
-function cancelAddLane() {
-    addLaneForm.style.display = 'none';
-}
+    let laneHeader = document.createElement('div')
+    laneHeader.className = 'lane-header'
+    laneHeader.tabIndex = 0
 
-function saveAddLane() {
-    const laneText = addLaneInput.value;
-    if (!laneText) return;
+    let laneTitle = document.createElement('span')
+    laneTitle.className = 'lane-title'
+    laneTitle.contentEditable = true
+    laneTitle.textContent = 'New Lane'
 
-    const newLane = document.createElement('div');
-    newLane.classList.add('lane');
-    newLane.innerHTML = `
-      <div class="lane-header">${laneText}</div>
-    `;
-    board.appendChild(newLane);
+    let removeButton = document.createElement('button')
+    removeButton.className = 'remove-lane-button'
+    removeButton.title = 'Remove Lane'
+    removeButton.textContent = 'X'
+    removeButton.onclick = function() {
+        removeLane(this)
+    }
 
-    const option = document.createElement('option');
-    option.value = board.children.length - 1;
-    option.innerText = laneText;
-    laneRemoveSelect.appendChild(option);
+    laneHeader.appendChild(laneTitle)
+    laneHeader.appendChild(removeButton)
 
-    addLaneForm.style.display = 'none';
-    addLaneInput.value = '';
+    let addItemButton = document.createElement('button')
+    addItemButton.className = 'add-item-button'
+    addItemButton.title = 'Add Item'
+    addItemButton.textContent = '+'
+    addItemButton.onclick = function() {
+        addItem(this)
+    }
+
+    newLane.appendChild(laneHeader)
+    newLane.appendChild(addItemButton)
+
+    board.appendChild(newLane)
     makeLaneDroppable(newLane)
+}
+
+function removeItem(button) {
+    let card = button.parentNode
+    card.parentNode.removeChild(card)
+}
+
+function removeLane(button) {
+    let lane = button.parentNode.parentNode
+    lane.parentNode.removeChild(lane)
 }
 
 board.addEventListener('dblclick', (event) => {
     if (event.target.classList.contains('card')) {
-        const card = event.target;
-        const lane = card.parentElement;
-        const laneIndex = Array.from(board.children).indexOf(lane);
-        let targetlaneIndex = laneIndex + 1;
+        const card = event.target
+        const lane = card.parentElement
+        const laneIndex = Array.from(board.children).indexOf(lane)
+        let targetlaneIndex = laneIndex + 1
         if (targetlaneIndex > board.children.length - 1) {
-            targetlaneIndex = 0;
+            targetlaneIndex = 0
         }
-        board.children[targetlaneIndex].appendChild(card);
+        board.children[targetlaneIndex].appendChild(card)
     }
-});
-
-function removeItem(button) {
-    var card = button.parentNode;
-    card.parentNode.removeChild(card);
-}
-
-function removeLane(button) {
-    var lane = button.parentNode.parentNode;
-    lane.parentNode.removeChild(lane);
-}
+})
 
 const makeItemDraggable = (item) => {
-    item.setAttribute('draggable', true);
+    item.setAttribute('draggable', true)
 
     item.addEventListener('dragstart', (event) => {
-        event.dataTransfer.setData('text/plain', event.target.innerText);
-    });
-};
+        event.dataTransfer.setData('text/plain', event.target.innerText)
+    })
+}
 
 const makeLaneDroppable = (lane) => {
     lane.addEventListener('dragover', (event) => {
-        event.preventDefault();
-    });
+        event.preventDefault()
+    })
 
     lane.addEventListener('drop', (event) => {
-        const data = event.dataTransfer.getData('text/plain');
-        const item = document.createElement('div');
-        item.classList.add('card');
-        item.innerText = data;
-        makeItemDraggable(item);
-        lane.appendChild(item);
-    });
-};
+        const data = event.dataTransfer.getData('text/plain')
+        const item = document.createElement('div')
+        item.classList.add('card')
+        item.innerText = data
+        makeItemDraggable(item)
+        lane.appendChild(item)
+    })
+}
 
-const items = document.querySelectorAll('.card');
-items.forEach((item) => makeItemDraggable(item));
+const items = document.querySelectorAll('.card')
+items.forEach((item) => makeItemDraggable(item))
 
-const lanes = document.querySelectorAll('.lane');
-lanes.forEach((lane) => makeLaneDroppable(lane));
+const lanes = document.querySelectorAll('.lane')
+lanes.forEach((lane) => makeLaneDroppable(lane))
